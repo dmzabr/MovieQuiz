21bbcf68eaeb6e635f84d232c364c93d7aaeb99d
Dmitriy Zabrodskiy <dzabrik@yandex.ru>
2024-10-17 10:50:48 +0300

    исправления после ревью - отдельно вынесен тест закрытия алерта

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -70,6 +70,23 @@ final class MovieQuizUITests: XCTestCase {
         XCTAssertEqual(indexLabel.label, "2/10")
     }
     
+    func testAlertClosing() {
+        sleep(3)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(1)
+        }
+        sleep(2)
+        let alert = app.alerts["endingAlert"]
+        XCTAssertTrue(alert.exists)
+        
+        alert.buttons.firstMatch.tap()
+        sleep(2)
+        
+        let indexLabel = app.staticTexts["Index"]
+        XCTAssertEqual(indexLabel.label, "1/10")
+    }
+    
     func testAlert() {
         sleep(3)
         for _ in 1...10 {
@@ -79,13 +96,13 @@ final class MovieQuizUITests: XCTestCase {
         sleep(2)
         let alert = app.alerts["endingAlert"]
         XCTAssertTrue(alert.exists)
-        XCTAssertTrue(alert.label == "Этот раунд окончен!")
-        XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть ещё раз")
+        XCTAssertEqual(alert.label, "Этот раунд окончен!")
+        XCTAssertEqual(alert.buttons.firstMatch.label, "Сыграть ещё раз")
         
         alert.buttons.firstMatch.tap()
         sleep(2)
         
         let indexLabel = app.staticTexts["Index"]
-        XCTAssertTrue(indexLabel.label == "1/10")
+        XCTAssertEqual(indexLabel.label, "1/10")
     }
 }
